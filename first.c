b61d4899bd9e4a7823c670473c5f4462beacbec0
Çağlar Doğan <dogancaglar05@gmail.com>
2020-10-10 21:33:51 +0300

    Add files via upload

--- first.c
+++ first.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-int main() {
-    int x;
-    printf("Enter a positive integer larger than 2:");
-    scanf("%d", &x);
-
-    if(x < 2) {
+int main(int argc, char *argv[]) {
+    
+    if(x < 2 || argc != 1) {
         printf("Error. Please enter a positive integer larger than 2 next time!");
         return 0;
     }
+
+    int x = argc[0];
 
     int numbers [x-1];
 
@@ -18,9 +17,10 @@ int main() {
 
     int counter = 0;
     int numprimes = 0;
+    int cprime;
 
     while(counter<=x-2){
-        int cprime = numbers[counter];
+        cprime = numbers[counter];
 
         if(cprime == -1){
             counter++;
@@ -40,16 +40,14 @@ int main() {
 
     printf("The number of primes smaller than %d: %d \n", x, numprimes);
 
+    int num;
+
     for(int i = 0; i <= x-2; i++){
-        int num = numbers[i];
+        num = numbers[i];
         if(num != -1){
             printf("%d\n",num);
         }
     }
-
-    char str[10];
-    printf("Press enter a string to close the app:");  
-    scanf("%s", str);
 
     return 0;
 }
